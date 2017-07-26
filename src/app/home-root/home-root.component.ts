04b8e35a1292dd9ff75165ba7b7a23156b6f8515
Nova-Prime <rawkstar.raju@gmail.com>
2017-07-27 04:09:47 +0530

    google charts Bar chart

--- src/app/home-root/home-root.component.ts
+++ src/app/home-root/home-root.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+declare var google: any;
 
 @Component({
   selector: 'app-home-root',
@@ -10,6 +11,38 @@ export class HomeRootComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+    if (document.getElementById('barchart_values')) {
+      google.charts.load('current', { packages: ['corechart'] });
+      google.charts.setOnLoadCallback(this.drawChart);
+
+
+    }
+  }
+  public drawChart() {
+    const data = google.visualization.arrayToDataTable([
+      ['City', '2010 Population'],
+      ['New York City, NY', 8175000],
+      ['Los Angeles, CA', 3792000],
+      ['Chicago, IL', 2695000],
+      ['Houston, TX', 2099000],
+      ['Philadelphia, PA', 1526000]
+    ]);
+
+    const options = {
+      title: 'Population of Largest U.S. Cities',
+      chartArea: { width: '50%' },
+      hAxis: {
+        title: 'Total Population',
+        minValue: 0
+      },
+      vAxis: {
+        title: 'City'
+      }
+    };
+
+    const chart = new google.visualization.BarChart(document.getElementById('barchart_values'));
+
+    chart.draw(data, options);
   }
 
 }
