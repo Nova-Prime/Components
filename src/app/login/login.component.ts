477046601f7be7d503c33ba0d56e8fd22e364c0c
Nova-Prime <rawkstar.raju@gmail.com>
2017-08-02 06:39:09 +0530

    Json schema validator

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -4,11 +4,30 @@ import { Logger } from '../components/Logger';
 
 import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 import { NgForm } from '@angular/forms/src/directives';
+import * as Ajv from 'ajv';
+
 // import * as data from './sample.json';
 
 
 declare var require: any;
 const x = require('./sample.json');
+const ajv = new Ajv({ $data: true });
+
+const schema = {
+  'properties': {
+    'smaller': {
+      'type': 'number',
+      'maximum': { '$data': '1/larger' }
+    },
+    'larger': { 'type': 'number' }
+  }
+};
+
+const validData = {
+  smaller: 5,
+  larger: '7'
+};
+
 
 
 @Component({
@@ -19,7 +38,7 @@ const x = require('./sample.json');
 })
 export class LoginComponent {
   gender: string;
-  obj: string;
+  obj: LocalPerson;
 
   constructor(private log: Logger, private http: Http) {
     console.log(x);
@@ -31,6 +50,8 @@ export class LoginComponent {
 
   oneButton() {
     alert('One clicked');
+    this.log.log(ajv.validate(schema, validData));
+
   }
   twoButton() {
     alert('two clicked');
