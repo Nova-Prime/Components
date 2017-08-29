db9ac6ad794d6c97175f2d43a26d3ea632f815c7
Nova-Prime <rawkstar.raju@gmail.com>
2017-08-29 06:39:27 +0530

    radio button modified & input component changed

--- src/app/components/input/input.component.ts
+++ src/app/components/input/input.component.ts
@@ -1,6 +1,6 @@
 import { ValueAccessor } from '../valueAccessor';
 import { Component, forwardRef, Input } from '@angular/core';
-import { NG_VALUE_ACCESSOR } from '@angular/forms';
+import { Form, NG_VALUE_ACCESSOR } from '@angular/forms';
 import { ControlValueAccessor, NgModel, NgModelGroup } from '@angular/forms/src/directives';
 
 @Component({
@@ -16,9 +16,48 @@ import { ControlValueAccessor, NgModel, NgModelGroup } from '@angular/forms/src/
   ]
 })
 export class InputComponent extends ValueAccessor {
-  @Input() id: String;
-  @Input() type: String;
-  @Input() lname: String;
-  @Input() name: String;
-  @Input() hint: String = '';
+  _readonly = false;
+  _disabled = false;
+  _isFocus = false;
+  _type = 'text';
+
+  @Input() name: string;
+  @Input() lname: string;
+
+  @Input() hint = '';
+  @Input() min: number | string = null;
+  @Input() max: number | string = null;
+  @Input() step: number | string = null;
+  @Input() autocomplete = '';
+  @Input() autocorrect = '';
+
+  @Input()
+  get readonly() {
+    return this._readonly;
+  }
+  set readonly(val: boolean) {
+    this._readonly = val;
+  }
+
+
+  @Input()
+  get type() {
+    return this._type;
+  }
+  set type(val: any) {
+    this._type = val;
+  }
+
+  @Input()
+  get disabled(): boolean {
+    return this._disabled;
+  }
+  set disabled(val: boolean) {
+    this._disabled = val;
+  }
+
+  onInput(ev: any) {
+    this.value = ev.target.value;
+  }
+
 }
